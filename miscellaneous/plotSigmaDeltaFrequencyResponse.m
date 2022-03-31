18c42acd9f487e076603dc89e1799f26ae1ecf6a
Jeffrey Thomsen <61237726+jeffrey-thomsen@users.noreply.github.com>
2022-03-31 16:22:51 +0200

    DAGA paper commit, run thomsen2022_main.m to generate the figures

--- miscellaneous/plotSigmaDeltaFrequencyResponse.m
+++ miscellaneous/plotSigmaDeltaFrequencyResponse.m
@@ -7,14 +7,18 @@ figure;
 hold on;
 
 [H,W] = freqz([1 0 0 0 0 +1], 1, 10000, 1000);
-plot(W,10*log10(abs(H)));
+plot(W,10*log10(abs(H)),'g',LineWidth=2);
 
 [H,W] = freqz([1 0 0 0 0 -1], 1, 10000, 1000);
-plot(W,10*log10(abs(H)));
+plot(W,10*log10(abs(H)),'r',LineWidth=2);
+
+xline(200,'-.',LineWidth=2)
+xline(400,'-.',LineWidth=2)
 
 xlim([0 500])
 ylim([-20 5])
-legend('\Sigma enhancement', '\Delta cancellation')
+legend('\Sigma enhancement', '\Delta cancellation', 'Location', ...
+    'northoutside','Orientation','horizontal')
 xlabel('Frequency (Hz)')
 ylabel('Magnitude (dB)')
 grid on
--- thomsen2022_main.m
+++ thomsen2022_main.m
@@ -121,7 +121,7 @@ enhancedSignalMixed = enhancedSignalMixed ./ ...
 
 evalMixed = evaluateAlgorithm(dataMixed.p0DetectedIndexVectors, AlgorithmParameters, ...
     dataMixed.p0SearchRangeSamplesVector, mixedSignal, timeVec, dataMixed.ivsMaskCells, ...
-    dataMixed.azimuthDegCells, dataMixed.targetSampleIndices, dataMixed.interfSampleIndices, centerFreqsHz);
+    dataMixed.azimuthDegCells, dataMixed.targetSampleIndices, dataMixed.interfSampleIndices, centerFreqsHz, false);
 
 %% Process target speech
 tic
@@ -133,7 +133,7 @@ enhancedSignalTarget = enhancedSignalTarget./max(max(abs(enhancedSignalTarget)))
 
 evalTarget = evaluateAlgorithm(dataTarget.p0DetectedIndexVectors, AlgorithmParameters, ...
     dataTarget.p0SearchRangeSamplesVector, targetSignal, timeVec, dataTarget.ivsMaskCells, ...
-    dataTarget.azimuthDegCells, dataTarget.targetSampleIndices, dataTarget.interfSampleIndices, centerFreqsHz);
+    dataTarget.azimuthDegCells, dataTarget.targetSampleIndices, dataTarget.interfSampleIndices, centerFreqsHz, false);
 
 p0SearchRangeHz = evalMixed.p0SearchRangeFreqVector;
 %% Process interferer speech
@@ -146,11 +146,9 @@ enhancedSignalInterf = enhancedSignalInterf./max(max(abs(enhancedSignalInterf)))
 
 evalInterf = evaluateAlgorithm(dataInterf.p0DetectedIndexVectors, AlgorithmParameters, ...
     dataInterf.p0SearchRangeSamplesVector, interfSignal, timeVec, dataInterf.ivsMaskCells, ...
-    dataInterf.azimuthDegCells, dataInterf.targetSampleIndices, dataInterf.interfSampleIndices, centerFreqsHz);
+    dataInterf.azimuthDegCells, dataInterf.targetSampleIndices, dataInterf.interfSampleIndices, centerFreqsHz, false);
 
-
-
-% %% Play mixed signals
+%% Play mixed signals
 % box1 = msgbox('Play original signal (Ensure volume is adequately set)');
 % waitfor(box1);
 % sound(mixedSignal, AlgorithmParameters.Gammatone.samplingRateHz);
@@ -205,49 +203,160 @@ nBands = AlgorithmParameters.Gammatone.nBands;
 
 metricTargetL = plotDistanceMetric(dataMixed.targetSampleIndices.L,dataTarget.targetSampleIndices.L,...
 dataMixed.p0DetectedIndexVectors.L,dataTarget.p0DetectedIndexVectors.L,...
-dataMixed.p0SearchRangeSamplesVector, nBands, centerFreqsHz, 'target', length(mixedSignal));
+dataMixed.p0SearchRangeSamplesVector, nBands, centerFreqsHz, 'target', length(mixedSignal), false);
 metricTargetR = plotDistanceMetric(dataMixed.targetSampleIndices.R,dataTarget.targetSampleIndices.R,...
 dataMixed.p0DetectedIndexVectors.R,dataTarget.p0DetectedIndexVectors.R,...
-dataMixed.p0SearchRangeSamplesVector, nBands, centerFreqsHz, 'target', length(mixedSignal));
+dataMixed.p0SearchRangeSamplesVector, nBands, centerFreqsHz, 'target', length(mixedSignal), false);
 
 metricInterfL = plotDistanceMetric(dataMixed.interfSampleIndices.L,dataInterf.interfSampleIndices.L,...
 dataMixed.p0DetectedIndexVectors.L,dataInterf.p0DetectedIndexVectors.L,...
-dataMixed.p0SearchRangeSamplesVector, nBands, centerFreqsHz, 'interferer', length(mixedSignal));
+dataMixed.p0SearchRangeSamplesVector, nBands, centerFreqsHz, 'interferer', length(mixedSignal), false);
 metricInterfR = plotDistanceMetric(dataMixed.interfSampleIndices.R,dataInterf.interfSampleIndices.R,...
 dataMixed.p0DetectedIndexVectors.R,dataInterf.p0DetectedIndexVectors.R,...
-dataMixed.p0SearchRangeSamplesVector, nBands, centerFreqsHz, 'interferer', length(mixedSignal));
+dataMixed.p0SearchRangeSamplesVector, nBands, centerFreqsHz, 'interferer', length(mixedSignal), false);
 
 
 nTotalTruePositivesT = sum(metricTargetL.nTruePositives)+...
                       sum(metricTargetR.nTruePositives);%+...
-nTotalTruePositivesI =                       sum(metricInterfL.nTruePositives)+...
+nTotalTruePositivesI = sum(metricInterfL.nTruePositives)+...
                       sum(metricInterfR.nTruePositives);
 
 nTotalFalsePositivesT = sum(metricTargetL.nFalsePositives)+...
                        sum(metricTargetR.nFalsePositives);%+...
-nTotalFalsePositivesI =                        sum(metricInterfL.nFalsePositives)+...
+nTotalFalsePositivesI = sum(metricInterfL.nFalsePositives)+...
                        sum(metricInterfR.nFalsePositives);
 
 nTotalFalseNegativesT = sum(metricTargetL.nFalseNegatives)+...
                        sum(metricTargetR.nFalseNegatives);%+...
-nTotalFalseNegativesI =                        sum(metricInterfL.nFalseNegatives)+...
+nTotalFalseNegativesI = sum(metricInterfL.nFalseNegatives)+...
                        sum(metricInterfR.nFalseNegatives);
 
-precision = nTotalTruePositives / ...
-    (nTotalTruePositives + nTotalFalsePositives);
-recall = nTotalTruePositives / ...
-    (nTotalTruePositives + nTotalFalseNegatives);
+precisionTarget = nTotalTruePositivesT / ...
+    (nTotalTruePositivesT + nTotalFalsePositivesT);
+precisionInterf = nTotalTruePositivesI / ...
+    (nTotalTruePositivesI + nTotalFalsePositivesI);
+recallTarget = nTotalTruePositivesT / ...
+    (nTotalTruePositivesT + nTotalFalseNegativesT);
+recallInterf = nTotalTruePositivesI / ...
+    (nTotalTruePositivesI + nTotalFalseNegativesI);
+
+precisionTotal = (nTotalTruePositivesT+nTotalTruePositivesI) / ...
+    (nTotalTruePositivesT + nTotalFalsePositivesT + nTotalTruePositivesI + nTotalFalsePositivesI);
 %%
-plotGlimpseOverlay(targetSignal, AlgorithmParameters.Gammatone.samplingRateHz, timeVec, nBands,...
-    dataTarget.targetSampleIndices, dataMixed.targetSampleIndices, p0SearchRangeHz,...
-    evalTarget.targetp0DetectedIndexVectors, evalMixed.targetp0DetectedIndexVectors, 'target')
+% plotGlimpseOverlay(targetSignal, AlgorithmParameters.Gammatone.samplingRateHz, timeVec, nBands,...
+%     dataTarget.targetSampleIndices, dataMixed.targetSampleIndices, p0SearchRangeHz,...
+%     evalTarget.targetp0DetectedIndexVectors, evalMixed.targetp0DetectedIndexVectors, 'target')
+
+%% subplot DOA histograms
+
+% figure('position',[2000 50 560 1260]);
+% s(1) = subplot(3,1,1);
+% doahistogram(evalTarget)
+% annotation('textbox','String',"a)",'FontSize',14,'LineStyle','none',...
+%     'Position',s(1).Position,'FitBoxToText','on')
+% s(2) = subplot(3,1,2);
+% doahistogram(evalInterf)
+% annotation('textbox','String',"b)",'FontSize',14,'LineStyle','none',...
+%     'Position',s(2).Position,'FitBoxToText','on')
+% s(3) = subplot(3,1,3);
+% doahistogram(evalMixed)
+% annotation('textbox','String',"c)",'FontSize',14,'LineStyle','none',...
+%     'Position',s(3).Position,'FitBoxToText','on')
+
+%% one plot DOA histograms
+
+figure;
+hold on;
+doahistogram(evalTarget,'#D95319')
+doahistogram(evalInterf,'#EDB120')
+doahistogram(evalMixed,'#0072BD')
+legend('Target speech signal', 'Interferer speech signal', 'Mixed speech signal')
+
+%% Sectrograms with period glimpse overlay
+subplot = @(m,n,p) subtightplot(m,n,p,[0.03 0.01], [0.08 0.05], [0.15 0.08]);
+% clear subplot
+
+figure('position',[2000 50 560 860]);
+s(1) = subplot(3,1,1);
+glimpsespectrogram(targetSignal, timeVec,...
+    AlgorithmParameters.Gammatone.samplingRateHz,...
+    AlgorithmParameters.Gammatone.nBands,...
+    evalTarget, dataTarget.targetSampleIndices, dataTarget.interfSampleIndices)
+annotation('textbox','String',"a)",'FontSize',14,'LineStyle','none',...
+    'Position',s(1).Position+[0 0.034 0 0],'FitBoxToText','on')
+xlabel('Time(s)','Color','none')
+set(gca,'xticklabels',[])
+s(2) = subplot(3,1,2);
+glimpsespectrogram(interfSignal, timeVec,...
+    AlgorithmParameters.Gammatone.samplingRateHz,...
+    AlgorithmParameters.Gammatone.nBands,...
+    evalInterf, dataInterf.targetSampleIndices, dataInterf.interfSampleIndices)
+annotation('textbox','String',"b)",'FontSize',14,'LineStyle','none',...
+    'Position',s(2).Position+[0 0.034 0 0],'FitBoxToText','on')
+xlabel('Time(s)','Color','none')
+set(gca,'xticklabels',[])
+s(3) = subplot(3,1,3);
+glimpsespectrogram(mixedSignal, timeVec,...
+    AlgorithmParameters.Gammatone.samplingRateHz,...
+    AlgorithmParameters.Gammatone.nBands,...
+    evalMixed, dataMixed.targetSampleIndices, dataMixed.interfSampleIndices)
+annotation('textbox','String',"c)",'FontSize',14,'LineStyle','none',...
+    'Position',s(3).Position+[0 0.034 0 0],'FitBoxToText','on')
+set(gca,'xticklabels',[])
+
+xticks(s(3),[0.5 1.5 2.5])
+xticklabels(s(3),{'0.5','1.5','2.5'})
 
 
 %% Auxiliary functions
 
+function [] = doahistogram(evaluation,colorCode)
+    histogram(nonzeros(evaluation.azDeg(abs(evaluation.azDeg)<90)),...
+        'Normalization','probability','NumBins',90,'FaceColor',colorCode)
+%     title(['DOA estimation histogram'])%,['total no. of subband samples: ',num2str(length(testSignal)*nBands)])
+%     ylabel({'No. of occurences';'in subband samples'})
+    ylabel('Probability of occurrence')
+    xlabel('$\hat{\varphi} (^{\circ})$','Interpreter','latex')
+    xticks([-90 -60 -30 0 30 60 90])
+%     ylim([0 0.25])
+    grid on
+    set(gca,'FontSize',14);
+end
+
+function [] = glimpsespectrogram(testSignal, timeVec, samplingRateHz, ...
+    nBands, evaluation, targetSampleIndices, interfSampleIndices)
+    meanTestSignal = (testSignal(:,1) + testSignal(:,2))./2;
+    winlen = 2000;
+    overlap = round(winlen-500);%kaiser5 *0.705);%blackmanharris *0.661);
+
+    spectrogram(meanTestSignal,blackmanharris(winlen),overlap,[],...
+        samplingRateHz,'yaxis','power');
+    colormap('bone');
+    ylim([0, 0.5]);
+    caxis([-80 -10])
+    hold on;
+    for iBand = 1:nBands%7
+        scatter(timeVec(targetSampleIndices.L{iBand}), ...
+            1e-3.*evaluation.p0SearchRangeFreqVector((evaluation.targetp0DetectedIndexVectors.L{iBand})), ...
+            10, 'green','filled');
+        scatter(timeVec(interfSampleIndices.L{iBand}), ...
+            1e-3.*evaluation.p0SearchRangeFreqVector((evaluation.interfp0DetectedIndexVectors.L{iBand})), ...
+            10, 'red','filled');
+
+        scatter(timeVec(targetSampleIndices.R{iBand}), ...
+            1e-3.*evaluation.p0SearchRangeFreqVector((evaluation.targetp0DetectedIndexVectors.R{iBand})), ...
+            10, 'green','filled');
+        scatter(timeVec(interfSampleIndices.R{iBand}), ...
+            1e-3.*evaluation.p0SearchRangeFreqVector((evaluation.interfp0DetectedIndexVectors.R{iBand})), ...
+            10, 'red','filled');
+        legend('target glimpses','interferer glimpses')
+    end
+    set(gca,'FontSize',14);
+end
+
 function evaluation = evaluateAlgorithm(p0DetectedIndexVectors, ...
     AlgorithmParameters, p0SearchRangeSamplesVector, testSignal, timeVec, ...
-    ivsMask, azimuthDegCells, targetSampleIndices, interfSampleIndices, fc)
+    ivsMask, azimuthDegCells, targetSampleIndices, interfSampleIndices, fc, Plotting)
 
     nBands = AlgorithmParameters.Gammatone.nBands;
         
@@ -301,6 +410,7 @@ function evaluation = evaluateAlgorithm(p0DetectedIndexVectors, ...
     evaluation.p0SearchRangeFreqVector = ...
         AlgorithmParameters.Gammatone.samplingRateHz./p0SearchRangeSamplesVector;
     
+    if Plotting
     figure;
     hBar = bar(evaluation.p0SearchRangeSecondsVector(evaluation.GR),...
         evaluation.GC);
@@ -309,11 +419,13 @@ function evaluation = evaluateAlgorithm(p0DetectedIndexVectors, ...
     xlabel('Period (s)')
     ylabel('No. of occurences in subband samples')
     set(gca,'FontSize',14);
+    end
     %% signal spectrogram
     meanTestSignal = (testSignal(:,1) + testSignal(:,2))./2;
     winlen = 2000;
     overlap = round(winlen-500);%kaiser5 *0.705);%blackmanharris *0.661);
     
+    if Plotting
     figure;
     spectrogram(meanTestSignal,blackmanharris(winlen),overlap,[],...
         AlgorithmParameters.Gammatone.samplingRateHz,'yaxis');
@@ -364,20 +476,24 @@ function evaluation = evaluateAlgorithm(p0DetectedIndexVectors, ...
         legend('periodic glimpses')
     end
     set(gca,'FontSize',14);
-
+    end
     %% plot azimuth estimation histogram
+    if Plotting
     figure;
-    histogram(nonzeros(evaluation.azDeg(abs(evaluation.azDeg)<90)))
+    histogram(nonzeros(evaluation.azDeg(abs(evaluation.azDeg)<90)),...
+        'Normalization','probability','NumBins',90)
     title(['DOA estimation histogram'])%,['total no. of subband samples: ',num2str(length(testSignal)*nBands)])
-    ylabel('No. of occurences in subband samples')
-    xlabel('azimuth (Degrees)')
+%     ylabel({'No. of occurences';'in subband samples'})
+    ylabel('Probability of occurrence')
+    xlabel('Azimuth (°)')
     xticks([-90 -60 -30 0 30 60 90])
+%     ylim([0 0.25])
     set(gca,'FontSize',14);
-
-
+    end
     %% plot position and p0 of detected target and interferer samples
 %     iStart = 1;
 %     iEnd = 30;
+%     if Plotting
 %     figure;
 %     for iBand = iStart:iEnd%[3,4,5,10,14,20,25,30]
 %         subplot(1,2,1)
@@ -405,13 +521,14 @@ function evaluation = evaluateAlgorithm(p0DetectedIndexVectors, ...
 %         ylim(flip(1./AlgorithmParameters.p0SearchRangeHz))
 %     end
 %     sgtitle({['Processed glimpses - Gammatone band no. ',num2str(iStart),'-',num2str(iEnd)],['fc=',num2str(fc(iStart),'%.0f'),'-',num2str(fc(iEnd),'%.0f'),'Hz']})
+%     end
 end
 
 
 
 function Metric = plotDistanceMetric(dataMixedSeparateSampleIndices, dataSeparateSeparateSampleIndices, ...
     dataMixedp0DetectedIndexVectors, dataSeparatep0DetectedIndexVectors, ...
-    p0SearchRangeSamplesVector, nBands, centerFreqsHz, separateString, lengthOfSignal)
+    p0SearchRangeSamplesVector, nBands, centerFreqsHz, separateString, lengthOfSignal, Plotting)
 
 
 truePositiveLogical = cell(1,nBands);
@@ -493,50 +610,52 @@ Metric.nOverallGlimpses(iBand) = numel(dataMixedSeparateSampleIndices{iBand});
 % end
 end
 end
-% figure;
-% scatter(centerFreqsHz,nSharedPeriods./nSharedGlimpses)
-% xlabel('gammatone center frequency (Hz)')
-% ylabel('ratio (1)')
-% title({['ratio of no. of shared ',separateString,' glimpses with identical period'],['to overall no. of shared ',separateString,' glimpses']})
+if Plotting
+    % figure;
+    % scatter(centerFreqsHz,nSharedPeriods./nSharedGlimpses)
+    % xlabel('gammatone center frequency (Hz)')
+    % ylabel('ratio (1)')
+    % title({['ratio of no. of shared ',separateString,' glimpses with identical period'],['to overall no. of shared ',separateString,' glimpses']})
 
-figure;
-hold on;
-bar(centerFreqsHz, Metric.nOverallGlimpses')
-bar(centerFreqsHz, Metric.nSharedGlimpses')
-bar(centerFreqsHz, Metric.nSharedPeriods')
-% scatter(centerFreqsHz,nSharedTargetPeriods)
-% scatter(centerFreqsHz,nSharedTargetSamples)
-xlabel('gammatone center frequency (Hz)')
-ylabel('no. of glimpses')
-legend(['overall no. of ',separateString,' glimpses in mixed signal'], ['no. of shared ',separateString,' glimpses'], ['no. of shared ',separateString,' glimpses with same p_0'])
-xlim([0 1500])
-title(['Comparing ',separateString,' glimpses in mixed and ',separateString,' signals'],['overall no. of samples per subband: ',num2str(lengthOfSignal)])
-set(gca,'FontSize',14);
-
-figure;
-hold on;
-% bar(centerFreqsHz,[Metric.precision; Metric.recall],1.6)
-bar(centerFreqsHz,Metric.precision)
-% bar(centerFreqsHz,Metric.recall)
-% scatter(centerFreqsHz, Metric.precision,25, 'red','filled')
-% scatter(centerFreqsHz, Metric.recall, 25, 'blue','filled')
-xlabel('gammatone center frequency (Hz)')
-ylabel('ratio (1)')
-legend([separateString,' precision'])%, [separateString,' recall'])
-xlim([0 1500])
-ylim([0 1])
-title(['Comparing ',separateString,' glimpses in mixed and ',separateString,' signals'])
-set(gca,'FontSize',14);
-
-% figure;
-% hold on
-% bar(centerFreqsHz,meanAbsDistanceOfTargetPeriodsInSamples)
-% % scatter(centerFreqsHz,nSharedTargetPeriods)
-% % scatter(centerFreqsHz,nSharedTargetSamples)
-% xlabel('gammatone center frequency (Hz)')
-% ylabel('mean distance (samples)')
-% title({['Mean absolute distance of detected periods'],['of shared ',separateString,' glimpses']})
-% xlim([0 1000])
+    figure;
+    hold on;
+    bar(centerFreqsHz, Metric.nOverallGlimpses')
+    bar(centerFreqsHz, Metric.nSharedGlimpses')
+    bar(centerFreqsHz, Metric.nSharedPeriods')
+    % scatter(centerFreqsHz,nSharedTargetPeriods)
+    % scatter(centerFreqsHz,nSharedTargetSamples)
+    xlabel('gammatone center frequency (Hz)')
+    ylabel('no. of glimpses')
+    legend(['overall no. of ',separateString,' glimpses in mixed signal'], ['no. of shared ',separateString,' glimpses'], ['no. of shared ',separateString,' glimpses with same p_0'])
+    xlim([0 1500])
+    title(['Comparing ',separateString,' glimpses in mixed and ',separateString,' signals'],['overall no. of samples per subband: ',num2str(lengthOfSignal)])
+    set(gca,'FontSize',14);
+    
+    figure;
+    hold on;
+    % bar(centerFreqsHz,[Metric.precision; Metric.recall],1.6)
+    bar(centerFreqsHz,Metric.precision)
+    % bar(centerFreqsHz,Metric.recall)
+    % scatter(centerFreqsHz, Metric.precision,25, 'red','filled')
+    % scatter(centerFreqsHz, Metric.recall, 25, 'blue','filled')
+    xlabel('gammatone center frequency (Hz)')
+    ylabel('ratio (1)')
+    legend([separateString,' precision'])%, [separateString,' recall'])
+    xlim([0 1500])
+    ylim([0 1])
+    title(['Comparing ',separateString,' glimpses in mixed and ',separateString,' signals'])
+    set(gca,'FontSize',14);
+    
+    % figure;
+    % hold on
+    % bar(centerFreqsHz,meanAbsDistanceOfTargetPeriodsInSamples)
+    % % scatter(centerFreqsHz,nSharedTargetPeriods)
+    % % scatter(centerFreqsHz,nSharedTargetSamples)
+    % xlabel('gammatone center frequency (Hz)')
+    % ylabel('mean distance (samples)')
+    % title({['Mean absolute distance of detected periods'],['of shared ',separateString,' glimpses']})
+    % xlim([0 1000])
+end
 
 end
 
